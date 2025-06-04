2e77e5e46f63dae25d800c8b55bf24fee638a695
badumbatish <tanghocle456@gmail.com>
2025-06-04 02:22:28 -0700

    ok

--- after_taste.sh
+++ after_taste.sh
@@ -2,4 +2,5 @@ curl -sS https://starship.rs/install.sh | sh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 
