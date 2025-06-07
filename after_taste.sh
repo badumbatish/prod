76eda885deb4f0f339419cdb428942975a656e20
badumbatish <tanghocle456@gmail.com>
2025-06-07 00:13:20 -0700

    Change to rust-installation packages

--- after_taste.sh
+++ after_taste.sh
@@ -1,9 +1,15 @@
+# Install starship
 curl -sS https://starship.rs/install.sh | sh
 
+# Install omzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Install atuin
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 
-ln -s $(which fdfind) ~/.local/bin/fd
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-cargo install git-delta
+# Install packages with rust
+cargo install fd-find git-delta ripgrep
+ln -s $(which fdfind) ~/.local/bin/fd
